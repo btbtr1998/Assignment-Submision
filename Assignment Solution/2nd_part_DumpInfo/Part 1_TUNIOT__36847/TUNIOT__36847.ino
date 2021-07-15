562a39cd34496a8ef9353c1352b6a46703b82a60
btbtr1998 <87478789+btbtr1998@users.noreply.github.com>
2021-07-15 17:54:54 +0530

    Add files via upload

---
+++ Assignment Solution/2nd_part_DumpInfo/Part 1_TUNIOT__36847/TUNIOT__36847.ino
@@ -0,0 +1,27 @@
+#include <AiEsp32RotaryEncoder.h>
+#include <AiEsp32RotaryEncoderNumberSelector.h>
+
+/////////////////////////////////
+// Generated with a lot of love//
+// with TUNIOT FOR ESP8266     //
+// Website: Easycoding.tn      //
+/////////////////////////////////
+
+
+void setup()
+{
+  Serial.begin(9600);
+
+}
+
+
+void loop()
+{
+
+    Serial.println("Temperature from sensor1 ");
+    Serial.println("Temperature from sensor2");
+    Serial.println("Temperature from sensor3");
+    Serial.println("Fan through transducer");
+    delay(60000);
+
+}
---
+++ Assignment Solution/TUNIOT__41869/TUNIOT__41869.ino
@@ -0,0 +1,29 @@
+#include <deprecated.h>
+#include <MFRC522.h>
+#include <MFRC522Extended.h>
+#include <require_cpp11.h>
+
+/////////////////////////////////
+// Generated with a lot of love//
+// with TUNIOT FOR ESP8266     //
+// Website: Easycoding.tn      //
+/////////////////////////////////
+
+
+void setup()
+{
+  Serial.begin(9600);
+
+}
+
+
+void loop()
+{
+
+    Serial.println("Temperature from sensor1 ");
+    Serial.println("Temperature from sensor2");
+    Serial.println("Temperature from sensor3");
+    Serial.println("Fan through transducer");
+    delay(60000);
+
+}
